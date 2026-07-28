9a8cf847345a9102cc49eb26450402d1696a85c5
Shahidul Islam <shahidul1920shakil@gmail.com>
2026-07-28 18:18:08 +0600

    lil

--- front-end/pages/Home.jsx
+++ front-end/pages/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
       {/* Main News Preview Panel */}
       <section className="mt-6 mb-12">
         <div className="container mx-auto px-4 md:px-6 grid gap-6 xl:grid-cols-[340px_minmax(0,1fr)] items-start">
-          <div className="left w-full xl:sticky xl:top-24 self-start">
+          <div className="left w-full order-2 xl:order-1 xl:sticky xl:top-24 self-start">
             <LeftSideNewsPnl />
           </div>
-          <div className="right w-full">
+          <div className="right w-full order-1 xl:order-2">
             <RightSideNewsPnl />
           </div>
         </div>
