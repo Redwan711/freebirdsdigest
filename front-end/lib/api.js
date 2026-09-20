1da1f7d60727fc5bc32b1fb879b638eda4b647df
Shahidul Islam <shahidul1920shakil@gmail.com>
2026-09-20 16:20:21 +0600

    fixes

--- front-end/lib/api.js
+++ front-end/lib/api.js
@@ -1,7 +1,11 @@
 // lib/api.js
 
 export async function fetchAPI(query, { variables } = {}) {
-  const headers = { 'Content-Type': 'application/json' };
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+  };
   
   const fetchOptions = {
     method: 'POST',
